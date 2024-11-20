b12a7ef444f749439f842372b0f258d77489cfc1
TheodoreMagna <theodore.magna@epitech.eu>
2024-11-20 15:25:17 +0100

    Fix symlink script

--- scripts/create_symlink_lisp.sh
+++ scripts/create_symlink_lisp.sh
@@ -1,7 +1,7 @@
 ls run-lisp
-echo "Creating symlink for run-lisp"
 if [ $? -eq 0 ]; then
     exit 0
 else
+    echo "Creating symlink for run-lisp"
     ln -s .stack-work/dist/x86_64-linux/ghc-9.6.6/build/lisp-exe/lisp-exe run-lisp
 fi
